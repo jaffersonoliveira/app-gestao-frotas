06d3b30aa7eb31e41f463db102ead3a3dcb4181a
jaffersondev <jafferson.oliveira@grupoadolfolutz.com.br>
2023-03-01 20:50:39 -0300

    improv getVehicles data

--- server/src/datas/vehicleData.ts
+++ server/src/datas/vehicleData.ts
@@ -2,7 +2,7 @@ import database from "../../db";
 
 export async function getVehicles() {
   const queryString = `
-    select veiculos.idveiculo, veiculos.placa, veiculos.veiculo, veiculos.combustivel, combustiveis.preco  
+    select veiculos.idveiculo, veiculos.placa, veiculos.veiculo, veiculos.combustivel, combustiveis.preco as preco_combustivel 
     from veiculos 
     left join combustiveis on veiculos.combustivel = combustiveis.combustivel`;
   const [rows] = await database.query(queryString);
