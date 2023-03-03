be28a402b1ebaa6c5f44de527912956b4fe6eab7
jaffersondev <jafferson.oliveira@grupoadolfolutz.com.br>
2023-03-03 10:56:57 -0300

    add fiel idcombustivel in getvehicles

--- server/src/datas/vehicleData.ts
+++ server/src/datas/vehicleData.ts
@@ -2,7 +2,7 @@ import database from "../../db";
 
 export async function getVehicles() {
   const queryString = `
-    select veiculos.idveiculo, veiculos.placa, veiculos.veiculo, combustiveis.combustivel, combustiveis.preco as preco_combustivel 
+    select veiculos.idveiculo, veiculos.placa, veiculos.veiculo, combustiveis.idcombustivel, combustiveis.combustivel, combustiveis.preco as preco_combustivel 
     from veiculos 
     left join combustiveis on veiculos.idcombustivel = combustiveis.idcombustivel`;
   const [rows] = await database.query(queryString);
